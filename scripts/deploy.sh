e787478621599dbfbbdcd62b7cb3fa620886f58c
Mattie Schraeder <mattie@mattie.lgbt>
2020-09-08 01:19:36 -0500

    add the version after the cd, not before

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ mattie@host.lgbt:~/discord/himitsubotsou
-ssh mattie@host.lgbt "cp ~/himitsubotsou.config.json ~/discord/himitsubotsou/config.json && sed -i '\$s/}/,\n\"version\":\"$TRAVIS_BRANCH\"}/' config.json && cd discord/himitsubotsou && npm install && pm2 restart index.js"
+ssh mattie@host.lgbt "cp ~/himitsubotsou.config.json ~/discord/himitsubotsou/config.json && cd discord/himitsubotsou && sed -i '\$s/}/,\n\"version\":\"$TRAVIS_BRANCH\"}/' config.json && npm install && pm2 restart index.js"
