91b2c324e5486ea4f4c349c2d726adca055aab65
Mattie Schraeder <mattie@mattie.lgbt>
2020-09-09 21:09:10 -0500

    missed a rename

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ mattie@host.lgbt:~/discord/himitsubotsou
-ssh mattie@host.lgbt "cp ~/himitsubotsou.config.json ~/discord/himitsubotsou/config.js && cd ~/discord/himitsubotsou && sed -i '\$s/}/,\n\"VERSION\":\"$TRAVIS_BRANCH\"}/' config.js && npm install && pm2 restart index.js"
+ssh mattie@host.lgbt "cp ~/himitsubotsou.config.js ~/discord/himitsubotsou/config.js && cd ~/discord/himitsubotsou && sed -i '\$s/}/,\n\"VERSION\":\"$TRAVIS_BRANCH\"}/' config.js && npm install && pm2 restart index.js"
