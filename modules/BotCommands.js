49b8b0cf7769005b79011bf5ee1c0b9fc81b32d7
Mattie Schraeder <mattie@mattie.lgbt>
2020-09-09 22:51:59 -0500

    ES6 is weird and makes finding javascript functions hard.

--- modules/BotCommands.js
+++ modules/BotCommands.js
@@ -6,9 +6,30 @@ export default class BotCommands {
     }
 
     help(args) {
-        var functions = this.#getAllMethodNames(this);
-
-        this.message.channel.send(functions.join(", "));
+        const getAllMethods = (obj) => {
+            let props = []
+        
+            do {
+                const l = Object.getOwnPropertyNames(obj)
+                    .concat(Object.getOwnPropertySymbols(obj).map(s => s.toString()))
+                    .sort()
+                    .filter((p, i, arr) =>
+                        typeof obj[p] === 'function' &&  //only the methods
+                        p !== 'constructor' &&           //not the constructor
+                        (i == 0 || p !== arr[i - 1]) &&  //not overriding in this prototype
+                        props.indexOf(p) === -1          //not overridden in a child
+                    )
+                props = props.concat(l)
+            }
+            while (
+                (obj = Object.getPrototypeOf(obj)) &&   //walk-up the prototype chain
+                Object.getPrototypeOf(obj)              //not the the Object prototype methods (hasOwnProperty, etc...)
+            )
+        
+            return props
+        }
+        
+        this.message.channel.send(getAllMethods(this).join(", "));
     }
 
     franelcrew(args) {
