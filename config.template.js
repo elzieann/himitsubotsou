e3d53fd545667a17090fd1fcf0acf4e1aff2ffe0
Mattie Schraeder <mattie@mattie.lgbt>
2020-10-17 00:22:55 -0500

    Add configuration for the bot to connect to a mysql database

--- config.template.js
+++ config.template.js
@@ -1,5 +1,9 @@
 export const Config = {
     "BOT_TOKEN": "YOUR TOKEN HERE",
     "PREFIX": "!",
-    "VERSION": "Filled in by Travis-CI or you can specify me. Whatever. You do you."
+    "VERSION": "Filled in by Travis-CI or you can specify me. Whatever. You do you.",
+    "MYSQL_HOST": "localhost",
+    "MYSQL_USER": "username",
+    "MYSQL_PASSWORD": "password",
+    "MYSQL_DATABASE": "database"
 }
